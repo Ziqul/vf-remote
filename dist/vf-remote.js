9aae2080f72be7e95ca39b991229285b8d38a95c
Maxim <maxim.sorokin@synebo.io>
2018-07-17 16:16:13 +0300

    Removed dist from .gitignore

---
+++ dist/vf-remote.js
@@ -0,0 +1,52 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.vfRemote = factory());
+}(this, (function () { 'use strict';
+
+    var classHandler = {
+        get: function(obj, prop) {
+            return new Proxy({prop: prop}, methodHandler);
+        }
+    };
+
+    var methodHandler = {
+        get: function(obj, prop) {
+            return function() {
+                var args = (
+                    arguments.length === 1 ?
+                    [arguments[0]] :
+                    Array.apply(null, arguments));
+
+                var methodName = obj.prop + '.' + prop;
+
+                return new Promise(function(resolve, reject) {
+                    var actionArgs = [methodName];
+
+                    actionArgs = actionArgs.concat(args);
+
+                    actionArgs = actionArgs.concat([function(result, event) {
+                        if (event.status) {
+                            resolve(result);
+                        } else {
+                            reject(event);
+                        }
+                    }]);
+
+                    actionArgs = actionArgs.concat(
+                        [{ buffer: true, escape: false, timeout: 30000 }]);
+
+                    console.log(actionArgs);
+
+                    Visualforce.remoting.Manager.invokeAction.apply(
+                        Visualforce.remoting.Manager, actionArgs);
+                });
+            };
+        }
+    };
+
+    var vfRemote = new Proxy({}, classHandler);
+
+    return vfRemote;
+
+})));
--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
-function function_name(argument) {
-    
+var methodHandler = {
+    get: function(obj, prop) {
+        return function() {
+            var args = (
+                arguments.length === 1 ?
+                [arguments[0]] :
+                Array.apply(null, arguments));
 
-    return null;
-}
+            var methodName = obj.prop + '.' + prop;
+
+            return new Promise(function(resolve, reject) {
+                var actionArgs = [methodName];
+
+                actionArgs = actionArgs.concat(args);
+
+                actionArgs = actionArgs.concat([function(result, event) {
+                    if (event.status) {
+                        resolve(result);
+                    } else {
+                        reject(event);
+                    }
+                }]);
+
+                actionArgs = actionArgs.concat(
+                    [{ buffer: true, escape: false, timeout: 30000 }]);
+
+                Visualforce
+                    .remoting.Manager
+                    .invokeAction.apply(
+                        Visualforce.remoting.Manager, actionArgs);
+            });
+        };
+    }
+};
+
+var classHandler = {
+    get: function(obj, prop) {
+        return new Proxy({prop: prop}, methodHandler);
+    }
+};
+
+var vfRemote = new Proxy({}, classHandler);
+
+export default vfRemote;
